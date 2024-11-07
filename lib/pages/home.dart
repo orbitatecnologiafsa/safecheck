41a0c9fa2509ca09670591045a5303c5029d950c
emal0n <edmundoneto511@hotmail.com>
2024-11-07 16:17:23 -0300

    lib:pages:home: Rebaseamento geral do código e melhorias

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:SafeCheck/elements/navbar.dart';
-import 'package:SafeCheck/util/smart_device_box.dart';
 
 class Main extends StatefulWidget {
   const Main({super.key});
@@ -14,21 +13,6 @@ class _Main extends State<Main> {
   
 final user = FirebaseAuth.instance.currentUser!;
 
-  List mySmartDevices = [
-    ["Inspeções", "assets/icons/inspecao.png"],
-    ["  Agenda", "assets/icons/agenda.png"],
-    ["Relatórios", "assets/icons/rel.png"],
-    ["Empresas e Funcionários", "assets/icons/em.png"],
-    ["Comunicados", "assets/icons/chat.png"],
-    ["Chat e Video", "assets/icons/chatevid.png"],
-  ];
-
-  // power button switched
-  void powerSwitchChanged(bool value, int index) {
-    setState(() {
-      mySmartDevices[index][2] = value;
-    });
-  }
   // sign user out method
   void signUserOut() {
     FirebaseAuth.instance.signOut();
@@ -46,10 +30,10 @@ final user = FirebaseAuth.instance.currentUser!;
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
         toolbarHeight: 100,
-        title: const Text('Home'),
-        titleTextStyle: const TextStyle( fontWeight: FontWeight.w900,
-        color: Colors.black,
-        fontSize: 30),
+        title: const Text('SafeCheck'),
+        titleTextStyle: const TextStyle(
+        color: Color(0xFF9CCC65),
+        fontSize: 20),
         centerTitle: true,
         backgroundColor: Colors.white,
         leading: Builder(
@@ -71,7 +55,6 @@ final user = FirebaseAuth.instance.currentUser!;
             child: Image.asset(
             'assets/icons/banner.png')
             ),
-
             Padding(padding: const EdgeInsets.only(left: 5.0),
             child: ListTile(
             leading: IconButton(
@@ -88,7 +71,6 @@ final user = FirebaseAuth.instance.currentUser!;
             ),
           ),
         ),
-
             Padding(padding: const EdgeInsets.only(left: 5.0),
             child: ListTile(
               leading: IconButton(
@@ -105,7 +87,6 @@ final user = FirebaseAuth.instance.currentUser!;
               ),
             ),
           ),
-
             Padding(padding: const EdgeInsets.only(left: 5.0),
             child: ListTile(
               leading: IconButton(
@@ -142,34 +123,315 @@ final user = FirebaseAuth.instance.currentUser!;
             ),
           ],
         )
-            ),
+      ),
+      
         body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 10),
-            Expanded(
-              child: GridView.builder(
-                itemCount: 6,
-                physics: const BouncingScrollPhysics(),
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 1 / 1.3,
-                ),
-                itemBuilder: (context, index) {
-                  return SmartDeviceBox(
-                    smartDeviceName: mySmartDevices[index][0],
-                    iconPath: mySmartDevices[index][1],
-                    onPressed: (value) => powerSwitchChanged(value, index),
-                  );
-                },
-              ),
-            )
+          child: GridView(children: [
+          InkWell(
+            onTap: (){
+            //  Navigator.push(context, MaterialPageRoute(builder: (context)=>const Main()));
+            },
+            child: Container(
+            decoration: const BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromARGB(255, 212, 210, 210),
+              offset: Offset(0, 0.5)
+            ),
           ],
         ),
+         child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40.0),
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                'assets/icons/inspecao.png',
+                height: 80,
+                color: const Color(0xFF9CCC65),
+              ),
+              const Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 0.1),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Inspeções',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          height: 1,
+                        ),
+                          ),
+                        ]
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],),
+            ),
+          ),
+          ),
+         InkWell(
+            onTap: (){
+          //    Navigator.push(context, MaterialPageRoute(builder: (context)=>const Soon()));
+            },
+            child: Container(
+            decoration: const BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromARGB(255, 212, 210, 210),
+              offset: Offset(0, 0.5)
+            ),
+          ],
+        ),
+         child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40.0),
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                'assets/icons/agenda.png',
+                height: 80,
+                color: const Color(0xFF9CCC65),
+              ),
+              const Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 0.1),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Agenda',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          height: 1,
+                        ),
+                          ),
+                        ]
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],),
+            ),
+          ),
+          ),
+          InkWell(
+            onTap: (){
+           //   Navigator.push(context, MaterialPageRoute(builder: (context)=>const Soon()));
+            },
+            child: Container(
+            decoration: const BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromARGB(255, 212, 210, 210),
+              offset: Offset(0, 0.5)
+            ),
+          ],
+        ),
+         child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40.0),
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                'assets/icons/rel.png',
+                height: 80,
+                color: const Color(0xFF9CCC65),
+              ),
+              const Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 0.1),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Relatórios',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          height: 1,
+                        ),
+                          ),
+                        ]
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],),
+            ),
+          ),
+          ),
+         InkWell(
+            onTap: (){
+          //    Navigator.push(context, MaterialPageRoute(builder: (context)=>const Soon()));
+            },
+            child: Container(
+            decoration: const BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromARGB(255, 212, 210, 210),
+              offset: Offset(0, 0.5)
+            ),
+          ],
+        ),
+         child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40.0),
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                'assets/icons/em.png',
+                height: 80,
+                color: const Color(0xFF9CCC65),
+              ),
+              const Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 0.1),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Cadastros',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          height: 1,
+                        ),
+                          ),
+                        ]
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],),
+            ),
+          ),
+          ),
+          InkWell(
+            onTap: (){
+       //       Navigator.push(context, MaterialPageRoute(builder: (context)=>const Soon()));
+            },
+            child: Container(
+            decoration: const BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromARGB(255, 212, 210, 210),
+              offset: Offset(0, 0.5)
+            ),
+          ],
+        ),
+         child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40.0),
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                'assets/icons/chat.png',
+                height: 80,
+                color: const Color(0xFF9CCC65),
+              ),
+              const Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 0.1),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Comunicados',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          height: 1,
+                        ),
+                          ),
+                        ],
+                      ),
+                      ),
+                  ),
+                ],
+              )
+            ],),
+            ),
+          ),
+          ),
+          InkWell(
+            onTap: (){
+      //        Navigator.push(context, MaterialPageRoute(builder: (context)=>const Soon()));
+            },
+            child: Container(
+            decoration: const BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromARGB(255, 212, 210, 210),
+              offset: Offset(0, 0.5)
+            ),
+          ],
+        ),
+         child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40.0),
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                'assets/icons/chatevid.png',
+                height: 80,
+                color: const Color(0xFF9CCC65),
+              ),
+              const Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 0.1),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Chat e Video',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          height: 1,
+                        ),
+                          ),
+                        ]
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],),
+            ),
+          ),
+          ),
+        ],
+        padding: const EdgeInsets.all(20),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 1 / 1.3,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        ),
+        ),
       ),
-    ),
+        ),
     );
   }
 }
